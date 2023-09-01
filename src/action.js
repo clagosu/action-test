13e22b44d15dfac861194cdbdd8811aa232923b0
clagosu <clagosu@users.noreply.github.com>
2023-09-01 01:50:18 -0400

    Create action.js

---
+++ src/action.js
@@ -0,0 +1,22 @@
+const = core = require('@actions/core');
+const github =require('@actions/github');
+
+async function run() {
+
+const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+
+const octokit = github.getOctokit(GITHUB_TOKEN);
+
+const workflows = await octokit.request(
+  "GET /repos/:owner/:repo/actions/workflows", 
+  { owner, repo }
+);
+  const workflow = workflows.data.workflows.find(
+  workflow => workflow.name === process.env.GITHUB_WORKFLOW
+);
+
+console.log(workflow);
+  
+}
+
+run();
